cd283d73d4a84ec3cb69dba5a06cc8e4304c44eb
willadenton <dentonwm@dukes.jmu.edu>
2021-10-13 13:48:48 -0400

    eqation in converters.py

--- converters.py
+++ converters.py
@@ -1,6 +1,13 @@
 """Library of units converters"""
 
-#
-# YOUR CODE GOES IN THIS FILE
-# Update the tests file to uncomment and run tests as you go
-#
+# define useful constants 
+NLB = 4.44822 # Newtons per pound-foot
+IPN = 1550    # square inches per sqare meter
+PKP = .001    # kilopascals per pascal 
+
+def psi2kpa(psi):
+    '''
+    Given a numeric value representing pounds per square inch (psi)
+    returns the equivalent value in kilopascals (kPa)
+    '''
+    return psi * NLB * IPN * PKP
